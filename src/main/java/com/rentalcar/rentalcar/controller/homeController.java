8470e6884fa2366236cb3c17f1b65c37fec7743a
NGUYEN HOANG LONG <longnhhe170400@fpt.edu.vn>
2024-10-27 01:19:19 +0700

    UI: Complete Add Car UI, Add My Cars page

--- src/main/java/com/rentalcar/rentalcar/controller/homeController.java
+++ src/main/java/com/rentalcar/rentalcar/controller/homeController.java
@@ -2,29 +2,13 @@ package com.rentalcar.rentalcar.controller;
 
 
 import com.rentalcar.rentalcar.dto.RegisterDto;
-import com.rentalcar.rentalcar.entity.User;
 import com.rentalcar.rentalcar.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-import com.rentalcar.rentalcar.dto.RegisterDto;
-import com.rentalcar.rentalcar.entity.User;
-import com.rentalcar.rentalcar.entity.VerificationToken;
-import com.rentalcar.rentalcar.exception.UserException;
-import com.rentalcar.rentalcar.repository.VerificationTokenRepo;
-import com.rentalcar.rentalcar.service.RegisterUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
