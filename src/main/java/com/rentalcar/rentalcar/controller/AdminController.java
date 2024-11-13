cdacd0677b143956dc08091e04047d33bf1d88a5
NGUYEN HOANG LONG <longnhhe170400@fpt.edu.vn>
2024-11-13 16:14:33 +0700

    Add Admin Dashboard

--- src/main/java/com/rentalcar/rentalcar/controller/AdminController.java
+++ src/main/java/com/rentalcar/rentalcar/controller/AdminController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/dashboard")
+    @GetMapping({"/admin/dashboard", "/admin"})
     public String adminDashboard() {
         return "/admin/index";
+    }
+
+    @GetMapping("/admin/user-list")
+    public String userList() {
+        return "/admin/app-user-list";
     }
 }
