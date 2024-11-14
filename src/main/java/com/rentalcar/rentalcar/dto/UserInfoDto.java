0d783f376848e6c7f6de9ad190b2c486f9168caa
minh <sktt1minh2003@gmail.com>
2024-11-14 21:31:58 +0700

    update license profile

--- src/main/java/com/rentalcar/rentalcar/dto/UserInfoDto.java
+++ src/main/java/com/rentalcar/rentalcar/dto/UserInfoDto.java
@@ -6,8 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
@@ -34,8 +34,8 @@ public class UserInfoDto {
         @Pattern(regexp = "^\\+?[0-9]{10}$", message = "Phone number must contain only numbers")
         private String phone;
 
-        @NotBlank(message = "Driving License ID is required")
-        private String drivingLicense;
+        private MultipartFile drivingLicense;
+        private String drivingLicensePath;
 
 
         private String city;
